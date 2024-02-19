f5d6a2f3e716ea745bd18c6e04b6955972a78dff
Fazida Ashraf Puthiyapurayil <FazidaAshraf_Puthiyapurayil@student.uml.edu>
2024-02-19 01:39:08 -0500

    test1.c

--- main.c
+++ main.c
@@ -9,7 +9,7 @@ void runChild(int childNum, int programNum) { // simulates child processes runni
     printf("Started child %d with pid %ld\n", childNum, (long)getpid());
 
     if (programNum == 1){
-            execlp("./test.c", "test1.c", NULL);
+            execlp("./test1.c", "test1.c", NULL);
             //if (execlp("./test1", "test1", NULL) == -1){
             //    perror("EXECLP ERROR!!!!!!!");
             printf("programNum is: %d\n", programNum);
